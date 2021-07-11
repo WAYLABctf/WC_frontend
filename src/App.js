3bb92ddd4ef3d7f7599f8e704c3f51125e1c3aa1
snwox <seonu64@gmail.com>
2021-07-11 20:12:24 +0900

    add pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React, { Component } from 'react';
+class App extends Component {
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://snwo.tistory.com"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -20,6 +19,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
